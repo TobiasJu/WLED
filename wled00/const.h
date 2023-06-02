61365e315a2f6bafa60c09a31527667667aa7c8d
Tobias <tobias.juhre@web.de>
2023-06-02 11:51:34 +0200

    RaveLED customizations

--- wled00/const.h
+++ wled00/const.h
@@ -7,8 +7,8 @@
 
 //Defaults
 #define DEFAULT_CLIENT_SSID "Your_Network"
-#define DEFAULT_AP_PASS     "wled1234"
-#define DEFAULT_OTA_PASS    "wledota"
+#define DEFAULT_AP_PASS     "rave1337"
+#define DEFAULT_OTA_PASS    "rave1337"
 
 //increase if you need more
 #ifndef WLED_MAX_USERMODS
